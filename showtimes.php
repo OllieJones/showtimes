582e34cb524a52b74a535d7181bb2cc387eddb02
ollie <olliejones@gmail.com>
2026-04-19 15:16:56 -0400

    Encapsulate in class to avoid global namespace pollution.

--- showtimes.php
+++ showtimes.php
@@ -16,175 +16,184 @@
 */
 
 namespace showtimes {
-    define( 'showtimes_KEY', 'Showtime' );
-    define( 'showtimes_NAME', 'showtime' );
-    define( 'showtimes_SLUG', 'show' );
 
-    add_action( 'init', function () {
-        add_filter( 'the_title', function ( $title, $post_id ) {
-            if ( ! $post_id ) {
-                return $title;
-            }
-            $found = get_category( $post_id, showtimes_SLUG );
-            if ( $found ) {
+    use DateTimeImmutable;
+    use Exception;
+    use WP_Term;
 
-                list( $showtime, $iso_showtime ) = get_showtime( $post_id, showtimes_KEY );
+    class Showtime {
 
-                if ( $showtime ) {
-                    $title = $showtime . ' ' . $title;
+        private $KEY = 'Showtime';
+        private $NAME = 'showtime';
+        private $SLUG = 'show';
+
+        public function __construct() {
+
+            add_filter( 'the_title', function ( $title, $post_id ) {
+                if ( ! $post_id ) {
+                    return $title;
                 }
+                $found = $this->get_category( $post_id, $this->SLUG );
+                if ( $found ) {
 
-            }
+                    list( $showtime, $iso_showtime ) = $this->get_showtime( $post_id, $this->KEY );
 
-            return $title;
-        }, 10, 2 );
-
-        if ( is_admin() ) {
-
-            $default = date( 'Y-m-d', strtotime( 'tomorrow' ) ) . 'T18:00:00';
-
-            register_post_meta( 'post', showtimes_KEY, array(
-                    'object_type'  => 'post',
-                    'show_in_rest' => true,
-                    'single'       => true,
-                    'type'         => 'string',
-                    'label'        => __( 'Showtime', 'showtimes' ),
-                    'description'  => __( 'The time and date of the event.', 'showtimes' ),
-            ) );
-
-            add_filter( 'manage_post_posts_columns', function ( $columns ) {
-                $columns[ showtimes_NAME ] = __( 'Showtime', 'showtimes' );
-
-                return $columns;
-            } );
-
-
-            add_action( 'manage_post_posts_custom_column', function ( $column, $post_id ) {
-                if ( 'post' === get_post_type( $post_id ) && get_category( $post_id, showtimes_SLUG ) ) {
-                    if ( $column === showtimes_NAME ) {
-                        list( $showtime, $isotime ) = get_showtime( $post_id, showtimes_KEY );
-                        $showtime = $showtime ?: '';
-                        echo esc_html( $showtime ) . '<span class="iso" data-iso="' . esc_attr( $isotime ) . '"></span>';
+                    if ( $showtime ) {
+                        $title = $showtime . ' ' . $title;
                     }
+
                 }
+
+                return $title;
             }, 10, 2 );
 
-            add_action( 'quick_edit_custom_box', function ( $column, $post_type ) {
-                if ( 'post' !== $post_type || showtimes_NAME !== $column ) {
+            if ( is_admin() ) {
+
+                $default = date( 'Y-m-d', strtotime( 'tomorrow' ) ) . 'T18:00:00';
+
+                register_post_meta( 'post', $this->KEY, array(
+                        'object_type'  => 'post',
+                        'show_in_rest' => true,
+                        'single'       => true,
+                        'type'         => 'string',
+                        'label'        => __( 'Showtime', 'showtimes' ),
+                        'description'  => __( 'The time and date of the event.', 'showtimes' ),
+                ) );
+
+                add_filter( 'manage_post_posts_columns', function ( $columns ) {
+                    $columns[ $this->NAME ] = __( 'Showtime', 'showtimes' );
+
+                    return $columns;
+                } );
+
+
+                add_action( 'manage_post_posts_custom_column', function ( $column, $post_id ) {
+                    if ( 'post' === get_post_type( $post_id ) && $this->get_category( $post_id, $this->SLUG ) ) {
+                        if ( $column === $this->NAME ) {
+                            list( $showtime, $isotime ) = $this->get_showtime( $post_id, $this->KEY );
+                            $showtime = $showtime ?: '';
+                            echo esc_html( $showtime ) . '<span class="iso" data-iso="' . esc_attr( $isotime ) . '"></span>';
+                        }
+                    }
+                }, 10, 2 );
+
+                add_action( 'quick_edit_custom_box', function ( $column, $post_type ) {
+                    if ( 'post' !== $post_type || $this->NAME !== $column ) {
+                        return;
+                    }
+                    ?>
+                    <fieldset class="inline-edit-col-right <?php echo esc_attr( $this->NAME ) ?>">
+                        <div class="inline-edit-col">
+                            <label>
+                                <span class="title"><?php _e( 'Showtime', 'showtimes' ); ?></span>
+                                <span class="input-text-wrap">
+							<input
+                                    type="datetime-local"
+                                    id="<?php echo esc_attr( $this->NAME ) ?>"
+                                    name="<?php echo esc_attr( $this->NAME ) ?>"
+                                    value=""/>
+						</span>
+                            </label>
+                        </div>
+                    </fieldset>
+                    <?php
+                }, 10, 2 );
+
+                add_action( 'save_post_post', function ( $post_id, $post, $update ) {
+                    $term = $this->get_category( $post_id, $this->SLUG );
+                    if ( ! $term ) {
+                        return;
+                    }
+                    if ( isset( $_POST[ $this->NAME ] ) ) {
+                        update_post_meta( $post_id, $this->KEY, sanitize_text_field( $_POST[ $this->NAME ] ) );
+                    }
+                }, 10, 3 );
+            }
+
+            add_action( 'admin_footer-edit.php', function () {
+                global $typenow;
+                if ( 'post' !== $typenow ) {
                     return;
                 }
                 ?>
-                <fieldset class="inline-edit-col-right <?php echo esc_attr( showtimes_NAME ) ?>">
-                    <div class="inline-edit-col">
-                        <label>
-                            <span class="title"><?php _e( 'Showtime', 'showtimes' ); ?></span>
-                            <span class="input-text-wrap">
-							<input
-                                    type="datetime-local"
-                                    id="<?php echo esc_attr( showtimes_NAME ) ?>"
-                                    name="<?php echo esc_attr( showtimes_NAME ) ?>"
-                                    value=""/>
-						</span>
-                        </label>
-                    </div>
-                </fieldset>
-                <?php
-            }, 10, 2 );
+                <script>
+                    jQuery(function ($) {
+                        if ('undefined' === typeof inlineEditPost) return
 
-            add_action( 'save_post_post', function ( $post_id, $post, $update ) {
-                $term = get_category( $post_id, showtimes_SLUG );
-                if ( ! $term ) {
-                    return;
-                }
-                if ( isset( $_POST[ showtimes_NAME ] ) ) {
-                    update_post_meta( $post_id, showtimes_KEY, sanitize_text_field( $_POST[ showtimes_NAME ] ) );
-                }
-            }, 10, 3 );
-        }
+                        const wp_inline_edit = inlineEditPost.edit
+                        inlineEditPost.edit = function (id) {
+                            wp_inline_edit.apply(this, arguments)
+                            const postId = ('object' === typeof (id)) ? parseInt(this.getId(id)) : 0
 
-        add_action( 'admin_footer-edit.php', function () {
-            global $typenow;
-            if ( 'post' !== $typenow ) {
-                return;
-            }
-            ?>
-            <script>
-                jQuery(function ($) {
-                    if ('undefined' === typeof inlineEditPost) return
-
-                    const wp_inline_edit = inlineEditPost.edit
-                    inlineEditPost.edit = function (id) {
-                        wp_inline_edit.apply(this, arguments)
-                        const postId = ('object' === typeof (id)) ? parseInt(this.getId(id)) : 0
-
-                        if (postId > 0) {
-                            const editRow = $('#edit-' + postId)
-                            const showtime = $('#post-' + postId).find('td.<?php echo esc_attr( showtimes_NAME )?> span.iso').data('iso')
-                            if ('string' === typeof showtime) {
-                                editRow.find('input[name="<?php echo esc_attr( showtimes_NAME )?>"]').val(showtime.trim())
-                            } else {
-                                editRow.find('fieldset.<?php echo esc_attr( showtimes_NAME )?>').hide()
+                            if (postId > 0) {
+                                const editRow = $('#edit-' + postId)
+                                const showtime = $('#post-' + postId).find('td.<?php echo esc_attr( $this->NAME )?> span.iso').data('iso')
+                                if ('string' === typeof showtime) {
+                                    editRow.find('input[name="<?php echo esc_attr( $this->NAME )?>"]').val(showtime.trim())
+                                } else {
+                                    editRow.find('fieldset.<?php echo esc_attr( $this->NAME )?>').hide()
+                                }
                             }
-                        }
-                    };
-                });
-            </script>
-            <?php
-        } );
+                        };
+                    });
+                </script>
+                <?php
 
-    } );
-
-    /** Get the WP_Term for a category slug if it exists.
-     *
-     * @param int $post_id The post id to look up.
-     * @param string $slug The metadata's slug to find.
-     *
-     * @return false|\WP_Term
-     */
-    function get_category( $post_id, $slug ) {
-        $categories = get_the_category( $post_id );
-
-        foreach ( $categories as $category ) {
-            if ( $slug === $category->slug ) {
-                return $category;
-            }
+            } );
         }
 
-        return false;
+        /** Get the WP_Term for a category slug if it exists.
+         *
+         * @param int $post_id The post id to look up.
+         * @param string $slug The metadata's slug to find.
+         *
+         * @return false|WP_Term
+         */
+        private function get_category( $post_id, $slug ) {
+            $categories = get_the_category( $post_id );
+
+            foreach ( $categories as $category ) {
+                if ( $slug === $category->slug ) {
+                    return $category;
+                }
+            }
+
+            return false;
+        }
+
+        /**
+         * @param int $post_id The post id to look up.
+         * @param string $meta_key The metadata to use. Default 'Showtime'
+         *
+         * @return array|false[] (Date-formatted string, iso string)
+         */
+        private function get_showtime( $post_id, $meta_key ) {
+            $showtime     = false;
+            $showtime_iso = get_post_meta( $post_id, $meta_key, true );
+
+            if ( $showtime_iso ) {
+                /* We have a stored time. */
+                try {
+                    $showtime    = new DateTimeImmutable( $showtime_iso, wp_timezone() );
+                    $date_format = get_option( 'date_format' ) . ' ' . get_option( 'time_format' );
+                    $showtime    = wp_date( $date_format, (int) $showtime->getTimestamp() );
+                    $showtime    = str_replace( ':00', '', $showtime );
+                } catch ( Exception $e ) {
+                    $showtime     = false;
+                    $showtime_iso = false;
+                }
+
+            }
+            if ( ! $showtime_iso ) {
+                /* Default ISO time. */
+                $showtime_iso = date( 'Y-m-d', strtotime( 'tomorrow' ) ) . 'T18:00:00';
+            }
+
+            return array( $showtime, $showtime_iso );
+
+        }
     }
-
-    /**
-     * @param int $post_id The post id to look up.
-     * @param string $meta_key The metadata to use. Default 'Showtime'
-     *
-     * @return array|false[] (Date-formatted string, iso string)
-     */
-    function get_showtime( $post_id, $meta_key ) {
-        $showtime     = false;
-        $showtime_iso = get_post_meta( $post_id, $meta_key, true );
-
-        if ( $showtime_iso ) {
-            /* We have a stored time. */
-            try {
-                $tzone    = get_option( 'timezone_string' );
-                $tzone    = $tzone ?: 'UTC';
-                $showtime = new \DateTimeImmutable( $showtime_iso, new \DateTimeZone( $tzone ) );
-            } catch ( \Exception $e ) {
-                $showtime = false;
-                $showtime_iso = false;
-            }
-            if ( $showtime ) {
-                $date_format = get_option( 'date_format' ) . ' ' . get_option( 'time_format' );
-                $showtime    = wp_date( $date_format, (int) $showtime->getTimestamp() );
-                $showtime    = str_replace( ':00', '', $showtime );
-            }
-        }
-        if ( ! $showtime_iso ) {
-            /* Default ISO time. */
-            $showtime_iso = $showtime_iso ?: date( 'Y-m-d', strtotime( 'tomorrow' ) ) . 'T18:00:00';
-        }
-
-        return array( $showtime, $showtime_iso );
-
-    }
+    add_action ( 'init', function () {
+       new Showtime();
+    });
 }
